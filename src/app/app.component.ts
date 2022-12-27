5fa1f48ec35d4f1f26e52af2ed916957ae5618e5
Jairo Cruz <azaveliz@example.com>
2022-12-26 18:56:59 -0600

    Login and Auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { MinValidator } from '@angular/forms';
+
+import { AuthService } from './services/auth.service';
+import { UsersService } from './services/users.service';
 
 
 
@@ -11,9 +15,34 @@ export class AppComponent {
   imgParent = '';
  
 
+
+
+  constructor(
+    private authService: AuthService,
+    private userService: UsersService,
+  ) {}
+
   onLoaded(img: string) {
     console.log('log desde el padre', img);
   }
 
+  createUser() {
+    this.userService.create({
+      name: 'jairo',
+      email: 'jairo@mail.com',
+      password: '1212'
+    })
+    .subscribe(rta => {
+      console.log(rta);
+    });
+  }
+
+
+  login() {
+    this.authService.login('jairo@mail.com','1212')
+    .subscribe(rta => {
+      console.log(rta.access_token);
+    });
+  }
 
 }
