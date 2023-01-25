3191f8b41ff8cad7405c7e1ca8182e1ba15f0066
Jairo Cruz <azaveliz@example.com>
2023-01-25 14:41:44 -0600

    add CMS module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -36,6 +36,10 @@ const routes: Routes = [
        },
     ]
   },
+  {
+    path: 'cms',
+    loadChildren: () => import('./cms/cms.module').then(m => m.CmsModule)
+  },
  {
    path: '**',
    component: NotFoundComponent
